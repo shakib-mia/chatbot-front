c219d84826e3ea7ca7e697e4a6ac311e4444d115
shakib-mia <smdshakibmia2001@gmail.com>
2023-02-19 13:11:51 +0600

    backend integration fully done

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// export const url = 'https://chabot-app-server.vercel.app';
-export const url = 'http://localhost:5000';
+export const url = 'https://chabot-app-server.vercel.app';
+// export const url = 'http://localhost:5000';
 
 export const getData = (setData, collection, body) => {
     fetch(`${url}/${collection}`, {
